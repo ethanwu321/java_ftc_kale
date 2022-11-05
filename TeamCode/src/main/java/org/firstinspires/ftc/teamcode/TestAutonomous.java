ebd4c4fe6059712b38b6229fa4236f9685609a23
Ethan Wu <ethanw2615@gmail.com>
2022-11-05 08:34:54 -0400

    First Autonomous Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="TestAutonomous")
+public class TestAutonomous extends LinearOpMode {
+    DcMotor frontRight;
+    DcMotor frontLeft;
+    DcMotor backRight;
+    DcMotor backLeft;
+
+    @Override
+    public void runOpMode() {
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        waitForStart();
+
+        frontLeft.setPower(0.4);
+        frontRight.setPower(0.4);
+        backLeft.setPower(0.4);
+        backRight.setPower(0.4);
+        sleep(500);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,7 +11,6 @@ public class TestTeleop extends OpMode {
     DcMotor frontLeft;
     DcMotor backRight;
     DcMotor backLeft;
-
 
     @Override
     public void init() {
@@ -25,15 +25,13 @@ public class TestTeleop extends OpMode {
 
     @Override
     public void loop() {
-        double x = gamepad1.left_stick_x;
-        double y = -gamepad1.left_stick_y;
-        double r = gamepad1.right_stick_x;
+        double x = gamepad1.left_stick_x * 0.4;
+        double y = -gamepad1.left_stick_y * 0.4;
+        double r = gamepad1.right_stick_x * 0.4;
 
-
-        frontLeft.setPower(y+r+x);
-        frontRight.setPower(y-r-x);
-        backLeft.setPower(y+r-x);
-        backRight.setPower(y-r+x);
-
+        frontLeft.setPower(y + r + x);
+        frontRight.setPower(y - r - x);
+        backLeft.setPower(y + r - x);
+        backRight.setPower(y - r + x);
     }
 }
