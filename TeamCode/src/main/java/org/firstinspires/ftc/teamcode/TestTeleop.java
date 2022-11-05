278470c0029cccbd3756cfe4605eecafd035d079
ethanwu321 <116760922+ethanwu321@users.noreply.github.com>
2022-11-05 09:11:47 -0400

    Update TestTeleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -1,12 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-public class TestTeleop extends OpMode {
+public class TestTeleOp extends OpMode {
     DcMotor frontRight;
     DcMotor frontLeft;
     DcMotor backRight;
