996fa77e3de1a4c50b1f8d16b67c514a69f54164
ethanwu321 <116760922+ethanwu321@users.noreply.github.com>
2022-11-05 12:44:34 -0400

    Update TestAutonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name="TestAutonomous")
+@Autonomous
 public class TestAutonomous extends LinearOpMode {
     DcMotor frontRight;
     DcMotor frontLeft;
