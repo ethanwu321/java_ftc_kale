a63b3b8e739ce622e76670eb7fc225d48203f1ec
Ethan Wu <ethanw2615@gmail.com>
2022-11-28 19:58:54 -0500

    Working Teleop + beginning to work on Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -2,14 +2,48 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name="TestAutonomous")
+@Autonomous
 public class TestAutonomous extends LinearOpMode {
     DcMotor frontRight;
     DcMotor frontLeft;
     DcMotor backRight;
     DcMotor backLeft;
+    CRServo Claw;
+
+    public void Motors_Forward(double power) {
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+    }
+    public void Motors_Backward(double power) {
+        frontLeft.setPower(-power);
+        frontRight.setPower(-power);
+        backLeft.setPower(-power);
+        backRight.setPower(-power);
+    }
+    public void Motors_Right(double power) {
+        frontLeft.setPower(power);
+        frontRight.setPower(-power);
+        backLeft.setPower(-power);
+        backRight.setPower(power);
+    }
+    public void Motors_Left(double power) {
+        frontLeft.setPower(-power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(-power);
+    }
+    public void Motors_TurnRight(double power){
+        frontLeft.setPower(power);
+        frontRight.setPower(-power);
+        backLeft.setPower(power);
+        backRight.setPower(-power);
+    }
+
 
     @Override
     public void runOpMode() {
@@ -22,11 +56,18 @@ public class TestAutonomous extends LinearOpMode {
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
 
-        frontLeft.setPower(0.4);
+        Motors_Forward(0.4);
+        sleep(300);
+        frontLeft.setPower(-0.4);
+        frontRight.setPower(-0.4);
+        backLeft.setPower(-0.4);
+        backRight.setPower(-0.4);
+        sleep(300);
+        frontLeft.setPower(-0.4);
         frontRight.setPower(0.4);
         backLeft.setPower(0.4);
-        backRight.setPower(0.4);
-        sleep(500);
+        backRight.setPower(-0.4);
+        sleep(700);
         frontLeft.setPower(0);
         frontRight.setPower(0);
         backLeft.setPower(0);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class TestTeleOp extends OpMode {
@@ -10,7 +11,8 @@ public class TestTeleOp extends OpMode {
     DcMotor frontLeft;
     DcMotor backRight;
     DcMotor backLeft;
-    float right_trigger;
+    // Servo rightclaw;
+    // Servo leftclaw;
 
     @Override
     public void init() {
@@ -18,32 +20,37 @@ public class TestTeleOp extends OpMode {
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        // leftclaw = hardwareMap.get(Servo.class, "LeftClaw");
+        // rightclaw = hardwareMap.get(Servo.class, "RightClaw");
 
         backRight.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
-
     }
 
     @Override
     public void loop() {
-        double x = gamepad1.left_stick_x * (gamepad1.right_trigger > 0.2 ? 0.1 : 0.7);
-        double y = -gamepad1.left_stick_y * (gamepad1.right_trigger > 0.2 ? 0.1 : 0.7);
-        double r = gamepad1.right_stick_x * (gamepad1.right_trigger > 0.2 ? 0.1 : 0.7);
+        double Lx = gamepad1.left_stick_x * (gamepad1.right_trigger > 0.2 ? 0.1 : 0.7);
+        double Ly = gamepad1.left_stick_y * (gamepad1.right_trigger > 0.2 ? 0.1 : 0.7);
+        double Rx = -gamepad1.right_stick_x * (gamepad1.right_trigger > 0.2 ? 0.1 : 0.7);
 
-        frontLeft.setPower(y + r + x);
-        frontRight.setPower(y + r - x);
-        backLeft.setPower(y - r - x);
-        backRight.setPower(y - r + x);
+        frontLeft.setPower(Ly + Rx - Lx);
+        frontRight.setPower(Ly - Rx + Lx);
+        backLeft.setPower(-Ly - Rx - Lx);
+        backRight.setPower(-Ly + Rx + Lx);
 
-        if (gamepad1.right_trigger > 0.2) {
-            x = gamepad1.left_stick_x * 0.1;
-            y = -gamepad1.left_stick_y * 0.1;
-            r = gamepad1.right_stick_x * 0.1;
-            frontLeft.setPower(y + r + x);
-            frontRight.setPower(y - r - x);
-            backLeft.setPower(y + r - x);
-            backRight.setPower(y - r + x);
+        telemetry.addData("front left power", frontLeft.getPower());
+        telemetry.addData("front right power", frontRight.getPower());
+        telemetry.addData("back left power", backLeft.getPower());
+        telemetry.addData("back right power", backRight.getPower());
+        telemetry.update();
 
-        }
+
+        // if(gamepad2.y)
+            // leftclaw.setPosition(0);
+            // rightclaw.setPosition(0);
+         // else if (gamepad2.x)
+            // leftclaw.setPosition(1);
+            // rightclaw.setPosition(1);
+
     }
 }
